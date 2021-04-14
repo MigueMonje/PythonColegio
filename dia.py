0628623c16ac73d7d94cbbd45c7bf75a841b6e44
Miguel Monje <mitomomu@gmail.com>
2021-04-14 08:43:48 -0400

    dia.py

---
+++ dia.py
@@ -0,0 +1,15 @@
+dias = {
+    1: "Lunes",
+    2: "Martes",
+    3: "Miercoles",
+    4: "Jueves",
+    5: "Viernes",
+    6: "Sabado",
+    7: "Domigo",
+    69: "SI"
+}
+d = int(input("Dia #: "))
+if d in dias:
+    print(f"El dia es {dias[d]}")
+else:
+    print("no")
